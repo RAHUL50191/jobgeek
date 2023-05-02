7a1503c4a7137afbe2aec6230589e89dc9e33712
Rahul <rahul200283@gmail.com>
2023-05-02 21:33:42 +0530

    update
    responsive and solving issues

--- JobGeek_v0.0.0/controller/homeController.js
+++ JobGeek_v0.0.0/controller/homeController.js
@@ -1,4 +1,5 @@
 const job = require("../model/jobModel");
+const Org = require("../model/orgModel");
 const fs = require("fs");
 const { default: mongoose } = require("mongoose");
 
@@ -7,4 +8,21 @@ const hometemp = fs.readFileSync(`${__dirname}/../public/home.html`);
 exports.getIndex = async (req, res) => {
   const jobData = await job.aggregate([{ $sample: { size: 3 } }]);
   res.render("homepage", { items: jobData });
+};
+
+exports.getInterview = async (req, res) => {
+  try {
+    const data = req.query;
+    const mentor = req.query.mentor;
+    const email = req.query.email;
+    console.log(data);
+    const orgdata = await Org.findOne({ email: email });
+    if (orgdata) {
+      res.render("interviewpage", { mentor: mentor });
+    } else {
+      res.status(401).send("Org not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
--- JobGeek_v0.0.0/router/homeRouter.js
+++ JobGeek_v0.0.0/router/homeRouter.js
@@ -7,4 +7,5 @@ router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 
 router.route("/home").get(homeCont.getIndex);
+router.route("/home/interview").get(homeCont.getInterview);
 module.exports = router;
