79a206f7dc39a0bebc8fe0876b26ba996ebbfa43
Rahul <rahul200283@gmail.com>
2023-05-06 11:06:12 +0530

    index
    responsive improve at home

--- JobGeek_v0.0.0/router/orgRouter.js
+++ JobGeek_v0.0.0/router/orgRouter.js
@@ -10,18 +10,9 @@ router.use(express.json());
 
 router.route("/signup").post(orgCont.register_org).get(orgCont.signupPage);
 router.route("/login").post(orgCont.loginAPI).get(orgCont.loginPage);
-router
-  .route("/forgot_password")
-  .post(orgCont.forgotPassword)
-  .get(orgCont.forgotPasswordPage);
-router
-  .route("/reset_password")
-  .post(orgCont.resetPassword)
-  .get(orgCont.resetPasswordPage);
-router
-  .route("/reset_email")
-  .post(auth, orgCont.resetEmail)
-  .get(orgCont.resetEmailPage);
+router.route("/forgot_password").post(orgCont.forgotPassword).get(orgCont.forgotPasswordPage);
+router.route("/reset_password").post(orgCont.resetPassword).get(orgCont.resetPasswordPage);
+router.route("/reset_email").post(auth, orgCont.resetEmail).get(orgCont.resetEmailPage);
 router.post("/update_email", orgCont.updateEmail);
 router.post("/update_avatar", orgCont.updateAvatar);
 router.route("/reset_avatar").post(auth, orgCont.updateAvatar);
